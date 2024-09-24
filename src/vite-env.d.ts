235ba0902374d4c5b4a4763fa33b6b7316ad605c
adseng <1983902539@qq.com>
2024-09-24 16:47:20 +0800

    init project

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="vite/client" />
+/// <reference types="@emotion/react/types/css-prop" />
+/// <reference types="vite-plugin-svgr/client" />
+declare global {
+    interface Window {
+        electronAPI: {
+            requestFullscreen: Function
+            exitFullscreen: Function
+            toggleFullscreen: Function
+            toggleDevTools: Function
+            isFullScreen: Function
+        }
+    }
+}
+
+export {}
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,67 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import terser from '@rollup/plugin-terser'
+import autoprefixer from 'autoprefixer'
+import tailwindcss from 'tailwindcss'
+import svgr from 'vite-plugin-svgr'
+import fs from 'fs'
 import electron from 'vite-plugin-electron'
 
-// https://vitejs.dev/config/
-export default defineConfig(({mode}) => {
+const version = fs.readFileSync(resolve(__dirname, 'doc/version')).toString().trim()
+process.env['VITE_BS_VERSION'] = version
+console.log('hello, 前端版本号:', process.env['VITE_NL_VERSION'])
 
-  return {
-    plugins:
-      [
-        react(),
-        mode === 'electron' ? electron({
-          entry: ['electron/main.ts', 'electron/preload.ts']
-        }) : null
-      ],
-  }
+// https://vitejs.dev/config/
+export default defineConfig((env) => {
+
+    return {
+        plugins: [
+            svgr(),
+            react({
+                jsxImportSource: '@emotion/react',
+                babel: {
+                    plugins: ['@emotion/babel-plugin']
+                }
+            }),
+            env.mode === 'pc' ? electron({
+                entry: ['electron/main.ts', 'electron/preload.ts']
+            }) : null
+        ],
+        resolve: {
+            alias: {
+                '@': resolve(__dirname, 'src')
+            }
+        },
+        base: '',
+        build: {
+            chunkSizeWarningLimit: 500,
+            rollupOptions: {
+                plugins: [
+                    terser({
+                        format: {
+                            comments: false
+                        }
+                    })
+                ],
+                output: {
+                    manualChunks: id => {
+                        if (id.includes('node_modules')) {
+                            return 'vendor'
+                        }
+                    }
+                }
+            }
+        },
+        css: {
+            preprocessorOptions: {
+                postcss: {
+                    plugins: [
+                        tailwindcss('./tailwind.config.js'),
+                        autoprefixer()
+                    ]
+                }
+            }
+        }
+    }
 })
